de45ac0529c68da5e8845c98f3f3ef9125ad144c
Jorge Hernandez <jnandez565@gmail.com>
2017-09-10 21:56:29 +0000

    create action done and rspec testing was also done for the create action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,4 +3,13 @@ class NotesController < ApplicationController
 
   end
 
+  def create
+    note = Note.create(note_params)
+    render json: note, status: :created
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+
 end
