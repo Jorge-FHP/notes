8051cc58942b91c44859afaaed0a06df7baac7c6
Jorge Hernandez <jnandez565@gmail.com>
2017-09-14 02:10:07 +0000

    tag validations testing done

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -2,17 +2,33 @@ require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
   describe "tags#create action" do
-   before do
-     @note = FactoryGirl.create(:note)
-     post :create, params: { tag: { name: 'Crazy' }, note_id: @note.id }
-   end
+    before do
+      @note = FactoryGirl.create(:note)
+      post :create, params: { tag: { name: 'Crazy' }, note_id: @note.id }
+    end
 
-   it "should return 200 status-code" do
-     expect(response).to be_success
-   end
+    it "should return 200 status-code" do
+      expect(response).to be_success
+    end
 
-   it "should successfully create and save a new tag in our database" do
-     expect(@note.tags.count).to eq(1)
-   end
- end
+    it "should successfully create and save a new tag in our database" do
+      expect(@note.tags.first.name).to eq('Crazy')
+    end
+  end
+
+  describe "tags#create action validations" do
+    before do
+      note = FactoryGirl.create(:note)
+      post :create, params: { tag: { name: '' }, note_id: note.id }
+    end
+
+    it "should properly deal with validation error" do
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "should return error json on validation error" do
+      json = JSON.parse(response.body)
+      expect(json['errors']['name'][0]).to eq("can't be blank")
+    end
+  end
 end
